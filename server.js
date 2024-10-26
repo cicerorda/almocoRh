89cf8851b9fb8bcb5e4f336996e4a935ab769bf9
cicerorda <cicero.rda@gmail.com>
2024-10-26 17:50:50 -0300

    correcao email

--- server.js
+++ server.js
@@ -77,7 +77,7 @@ function enviarEmailDiario() {
 
     const mailOptions = {
         from: `"No Reply" <${process.env.GMAIL_USER}>`,
-        to: '',
+        to: 'ttcicero@gmail.com',
         bcc: 'ttcicero@gmail.com',
         subject: 'Relatório Diário de Pedidos de Refeição',
         text: 'Segue em anexo o relatório de pedidos de refeições recentes.',
